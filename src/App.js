2c887969ce789e3d90a262bebec383ee582c2f28
Sepehr hashemi <sepehrhashemimamaquani1996@gmail.com>
2022-05-17 18:13:10 +0430

    build a calculator app in react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import DigitButton from "./components/DigitButton";
+import {useReducer} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const Actions = {
+    DIGIT: 'ADD_DIGIT',
+    OPERATION: 'ADD_OPERATION',
+    CLEAR: 'CLEAR',
+    DELETE: 'DELETE',
+    EVALUATION: 'EVALUATION'
 }
+const defaultReducer = {
+    operation: "",
+    currentOperand: "",
+    previousOperand: ""
+}
+const numberFormat = new Intl.NumberFormat('en-us', { maximumFractionDigits: 0});
+function formatOperand(operand) {
+    if (operand === "" || operand == null) return
+    const [integer, decimal] = operand.split('.');
+    if (decimal == null || decimal === "") return numberFormat.format(integer)
+    return `${numberFormat.format(integer)}.${decimal}`
+}
+const calculatorReducer = (state, action) => {
+    switch (action.type) {
+        case Actions.DIGIT:
+            if (state.overwrite) {
+                return {
+                    ...state,
+                    currentOperand: action.payload,
+                    overwrite: false
+                }
+            }
+            if (action.payload === "0" && state.currentOperand === "0") return state
+            if (action.payload === "." && state.currentOperand.includes(".")) return state
+            return {
+                ...state,
+                currentOperand: `${state.currentOperand}${action.payload}`
+            }
+        case Actions.DELETE:
+            if (state.overwrite) {
+                return {
+                    ...state,
+                    overwrite: false,
+                    currentOperand: ""
+                }
+            }
+            if (state.currentOperand === "") return state
+            if (state.currentOperand.length === 1) return { ...state, currentOperand: ""}
+            return {
+                ...state,
+                currentOperand: `${state.currentOperand.slice(0, -1)}`
+            }
+        case Actions.CLEAR:
+            return defaultReducer
+        case Actions.OPERATION:
+            if (state.currentOperand === "") {
+                return {
+                    ...state,
+                    operation: action.payload
+                }
+            }
+            if (state.currentOperand.length === 0 && state.previousOperand.length === 0) return state
+            if (state.previousOperand.length === 0) {
+                return {
+                    ...state,
+                    operation: action.payload,
+                    previousOperand: state.currentOperand,
+                    currentOperand: ""
+                }
+            }
+            return {
+                ...state,
+                previousOperand: evaluation(state),
+                overwrite: true,
+                operation: action.payload,
+                currentOperand: ""
+            }
+        case Actions.EVALUATION:
+            if (state.operation === "" || state.currentOperand === "" || state.previousOperand === "") {
+                return state
+            }
+            console.log(state)
+            return {
+                ...state,
+                previousOperand: "",
+                overwrite: true,
+                operation: "",
+                currentOperand: evaluation(state)
+            }
+        default:
+            return defaultReducer
+    }
+}
+function evaluation({ previousOperand, currentOperand, operation}) {
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return ""
+    let computation = "";
+    switch (operation) {
+        case "+":
+            computation = prev + current
+            break;
+        case "-":
+            computation = prev - current
+            break;
+        case "*":
+            computation = prev * current
+            break;
+        case "÷":
+            computation = prev / current
+            break;
+        default:
+            computation = ""
+    }
+    return computation.toString()
+}
+const App = () => {
+    const [calculatorState, dispatch] = useReducer(calculatorReducer, defaultReducer)
+    return(
+        <div className="calculator">
+            <div className="calculator__output">
+                <div className="calculator__output--previous">{formatOperand(calculatorState.previousOperand)} {calculatorState.operation}</div>
+                <div className="calculator__output--current">{formatOperand(calculatorState.currentOperand)}</div>
+            </div>
+            <DigitButton dispatch={dispatch}/>
+        </div>
+    )
+};
 
 export default App;
